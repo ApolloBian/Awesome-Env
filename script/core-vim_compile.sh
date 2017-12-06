4edd53559990fcfbe9817a85a4f427a1864a0eb4
Tianling@ApoMarkI.local <bian_tianling@sjtu.edu.cn>
2017-12-06 21:57:48 +0800

    Complete most work on Darwin

--- script/core-vim_compile.sh
+++ script/core-vim_compile.sh
@@ -1,29 +1,38 @@
 # Some futher references:
 # http://liuchengxu.org/posts/use-vim-as-a-python-ide/
-# TODO: multi-platform support
 
 
-
+# Setup compile env variable according to plafrorm
 # compile vim
 if [[ `uname` == 'Darwin' ]]; then
     # Set compile flags here
     num_proc=8
+    prefix=$HOME/Applications/
 else
-    python_config_dir=/usr/bin/python2.7/$(ls /usr/bin/python2.7 | grep ^config)
-    num_proc=$(cat /proc/cpuinfo| grep "processor"| wc -l)
+    num_proc=`cat /proc/cpuinfo| grep "processor"| wc -l`
+    prefix=$HOME/.local
 fi
+# python_config_dir=/usr/lib/python2.7/`ls /usr/lib/python2.7 | grep ^config`
+# echo $python_config_dir
 echo $num_proc
+echo $prefix
+
+
 cd $HOME/oh-my-dotfiles/resources/vim
+make uninstall
 ./configure --with-features=huge \
     --enable-multibyte \
     --enable-rubyinterp=yes \
     --enable-pythoninterp=yes \
-    --with-python-config-dir=$python_config_dir \
+    --enable-python3interp=yes \
     --enable-perlinterp=yes \
     --enable-luainterp=yes \
     --enable-cscope \
-    --prefix=$HOME/.local/
+    --enable-gui=gtk2,gnome,gtk3 \
+    --prefix=$prefix
+#     --disable-darwin \
+#     --with-python-config-dir=$python_config_dir \
 
-#     --enable-gui=gtk2 \
-make -j$(num_proc)
+make clean
+make -j`num_proc`
 make install
