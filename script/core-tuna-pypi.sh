beef69ac4b83b1f7d430a69051b4774ef2a46b81
Tianling@ApoMarkI.local <bian_tianling@sjtu.edu.cn>
2018-01-01 16:10:22 +0800

    fix careless mistakes

--- script/core-tuna-pypi.sh
+++ script/core-tuna-pypi.sh
@@ -1,12 +1,11 @@
 # pip
 if [[ `uname` == 'Darwin' ]]; then
     # Darwin
-    pipdir=$HOME/Library/Applicatiou\ Support/pip/
-    mkdir -p $pipdir
+    pipdir=$HOME/Library/Application\ Support/pip
 else
-    pipdir=$HOME/.config/pip/
-    mkdir -p $pipdir
+    pipdir=$HOME/.config/pip
 fi
-pipconf=$pipdir/pip.conf
-echo '[global]' > $pipconf
-echo 'index-url = https://pypi.tuna.tsinghua.edu.cn/simple' >> $pipconf
+mkdir -p $pipdir
+pipconfig=$pipdir/pip.conf
+echo '[global]' > "$pipconfig"
+echo 'index-url = https://pypi.tuna.tsinghua.edu.cn/simple' >> "$pipconfig"
