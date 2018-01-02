438818e19874453951ee4df74fe63a8f6506ef83
Tianling@ApoMarkI.local <bian_tianling@icloud.com>
2018-01-02 14:22:40 +0800

    remove deprecated airboot.sh

--- airboot.sh
+++
@@ -1,7 +0,0 @@
-dir="$HOME/oh-my-dotfiles"
-echo "Cloning oh-my-dotfiles"
-git clone --recursive git@github.com:ApolloBian/oh-my-dotfiles.git
-git submodule update --init --recursive
-
-cd $dir
-./install.py
