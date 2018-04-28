85425222db3b211254f8c18329f1d9c0c0d411e4
Tianling@AISIG02 <bian_tianling@icloud.com>
2018-04-29 04:15:52 +0800

    fix reinstallation issue for linuxbrew

--- script/server-brew.sh
+++ script/server-brew.sh
@@ -2,6 +2,9 @@ source resources/dotfiles/env
 which brew
 if [[ $? != 0 ]]; then
     # install a tmp version of ruby 2.3.5
+    rm -rf /tmp/tianling/
+    rm -rf /home/linuxbrew/.linuxbrew
+    rm -rf $HOME/.linuxbrew
     mkdir -p /tmp/tianling/
     cd /tmp/tianling/
     wget https://cache.ruby-lang.org/pub/ruby/2.3/ruby-2.3.5.tar.gz
