369edddc2edf51369ef80bb010d3f30274b6178f
Tianling@ApoMarkX.local <bian_tianling@icloud.com>
2018-07-19 12:22:29 +0800

    update brew pacakges

--- script/darwin-brew_package.sh
+++ script/darwin-brew_package.sh
@@ -1,4 +1,4 @@
 brew update
-brew install htop tmux cmake git fcitx-remote-for-osx wget python3 npm ctags
+brew install htop tmux cmake git fcitx-remote-for-osx wget python3 python2 npm ctags pyenv
 brew cask install skim iterm2
 brew upgrade
