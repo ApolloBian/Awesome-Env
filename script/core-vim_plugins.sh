fbaaf23e39a28e527cb7c99ff210e999b5016266
Tianling@AISIG01 <bian_tianling@icloud.com>
2018-08-04 04:58:31 +0800

    remove ycm js completion

--- script/core-vim_plugins.sh
+++ script/core-vim_plugins.sh
@@ -10,7 +10,7 @@ git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouComplet
 cd ~/.vim/bundle/YouCompleteMe
 git submodule update --init --recursive
 
-./install.py --clang-completer --js-completer
+./install.py --clang-completer
 # uname -a | grep -i ubuntu
 # if [[ $? != 0 ]]; then
 #     ./install.py --clang-completer --system-libclang
