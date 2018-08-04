322b88832fd62223776fce90c3fc7e271acc953e
Tianling@ApoMarkI.local <bian_tianling@icloud.com>
2018-08-04 18:11:16 +0800

    add neofetch 4.0.1 installation/config script

---
+++ script/core-neofetch.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+neo_config_root=~/.config/neofetch
+
+# 1. make config dir
+mkdir -p $neo_config_root
+mkdir -p $neo_config_root/img
+# 2. link files (config and img)
+
+ln -sf $(pwd)/resources/neofetch_config.conf $neo_config_root/config.conf
+ln -sf $(pwd)/resources/pictures/neofetch_* $neo_config_root/img/
+
+# 3. install
+if [[ `uname` == 'Darwin' ]]; then
+    prefix=$HOME/Applications/
+else
+    prefix=$HOME/.local
+fi
+workdir=$(pwd)
+cd $(pwd)/resources/neofetch/
+make PREFIX=$prefix install
+cd $workdir
+echo "neofetch" >> ~/.local_env
