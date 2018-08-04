aaee0e9b54875c8374e2fe6abd3d9a6702095cd5
Tianling@ApoMarkI.local <bian_tianling@icloud.com>
2018-08-04 11:52:18 +0800

    organize darwin-brew_package

--- script/darwin-brew_package.sh
+++ script/darwin-brew_package.sh
@@ -1,6 +1,8 @@
 brew update
+# some useful dev tools
 brew install htop tmux cmake git fcitx-remote-for-osx wget python3 python2 npm ctags pyenv
 # neofetch deps
 brew install screenresolution neofetch
+# useful apps
 brew cask install skim iterm2
 brew upgrade
