c9e2634b9a762aa24a83e6b68cc44b6eec9c05cf
Tianling@ApoMarkI.local <bian_tianling@icloud.com>
2018-08-04 11:50:03 +0800

    install neofetch

--- script/darwin-brew_package.sh
+++ script/darwin-brew_package.sh
@@ -1,4 +1,6 @@
 brew update
 brew install htop tmux cmake git fcitx-remote-for-osx wget python3 python2 npm ctags pyenv
+# neofetch deps
+brew install screenresolution neofetch
 brew cask install skim iterm2
 brew upgrade
