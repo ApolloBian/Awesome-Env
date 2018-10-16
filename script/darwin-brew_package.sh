8a155ee55235afc15137c357790f2fb2fc3fbaa9
Tianling@ApoMarkI.local <bian_tianling@icloud.com>
2018-10-16 20:13:23 +0800

    update homebrew package

--- script/darwin-brew_package.sh
+++ script/darwin-brew_package.sh
@@ -3,6 +3,10 @@ brew update
 brew install htop tmux cmake git fcitx-remote-for-osx wget python3 python2 npm ctags pyenv
 # neofetch deps
 brew install screenresolution neofetch
+# gnu coreutils (including ls)
+brew install coreutils
 # useful apps
 brew cask install skim iterm2
+brew tap homebrew/cask-versions
+brew cask install iterm2
 brew upgrade
