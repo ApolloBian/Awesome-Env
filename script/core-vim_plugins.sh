fbefe30b66c98e29df0f496ba4f8f14181f4331a
Tianling@ApoMarkX.local <bian_tianling@icloud.com>
2019-01-15 18:54:55 +0800

    migrate from vundle to vim-plug

--- script/core-vim_plugins.sh
+++ script/core-vim_plugins.sh
@@ -2,25 +2,18 @@
 # http://liuchengxu.org/posts/use-vim-as-a-python-ide/
 # TODO: multi-platform support
 
-# setting up vim plugins
-source ~/.env
-rm -rf ~/.vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
-cd ~/.vim/bundle/YouCompleteMe
-git submodule update --init --recursive
+if [[ `uname` == 'Darwin' ]]; then
+    num_proc=8
+else
+    num_proc=`cat /proc/cpuinfo| grep "processor"| wc -l`
+fi
+echo $num_proc
+source $HOME/.env
+rm -rf $HOME/.vim/plugged
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-./install.py --clang-completer
-# uname -a | grep -i ubuntu
-# if [[ $? != 0 ]]; then
-#     ./install.py --clang-completer --system-libclang
-# else
-#     # download clang if on server
-#     ./install.py --clang-completer
-# fi
-
-
-vim +PluginInstall
+vim +PlugInstall $num_proc
 # if [[ `uname` == 'Darwin' ]]; then
 #     cp $HOME/.vim/bundle/fcitx.vim/so/fcitx.vim $HOME/.vim/bundle/fcitx.vim/plugin/
 # fi
