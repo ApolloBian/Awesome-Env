092cbf73189b3cd6c25d21e7084d8b5eff5d7052
Tianling@ApoMarkX.local <bian_tianling@icloud.com>
2019-01-16 16:31:59 +0800

    update vim related settings; refactor vimrc

--- script/core-vim_compile.sh
+++ script/core-vim_compile.sh
@@ -28,7 +28,6 @@ make clean
 ./configure --with-features=huge \
     --enable-multibyte \
     --enable-rubyinterp=yes \
-    --enable-pythoninterp=yes \
     --enable-python3interp=yes \
     --enable-perlinterp=yes \
     --enable-luainterp=yes \
