b8d7f3d578b8c94a33b596888b81d0d5793f3615
Tianling@Octopus <bian_tianling@icloud.com>
2020-07-23 22:54:40 +0800

    add Retro theme for blink shell

---
+++ resources/Retro.blink.js
@@ -0,0 +1,26 @@
+black       = '#0c0c0c'; // black
+lightBlack  = '#b8b5ad'; // medium gray
+red         = '#b71414'; // red
+lightRed    = '#cf6d6d'; // red
+green       = '#0e816e'; // green
+lightGreen  = '#11a891'; // green
+yellow      = '#818d96'; // yellow
+lightYellow = '#818d96'; // yellow
+blue        = '#0e3e81'; // blue
+lightBlue   = '#1458b8'; // blue
+magenta     = '#0e3e81'; // pink
+lightMagenta= '#1458b8'; // pink
+cyan        = '#0e816e'; // cyan
+lightCyan   = '#11a891'; // cyan
+white       = '#f4f4f4'; // light gray
+lightWhite  = '#f4f4f4'; // white
+
+t.prefs_.set('color-palette-overrides',
+                 [ black , red     , green  , yellow,
+                  blue     , magenta , cyan   , white,
+                  lightBlack   , lightRed  , lightGreen , lightYellow,
+                  lightBlue    , lightMagenta  , lightCyan  , lightWhite ]);
+
+t.prefs_.set('cursor-color', 'rgba(0, 0, 0, 0.5)');
+t.prefs_.set('foreground-color', '#0c0c0c');
+t.prefs_.set('background-color', '#f4f4f4');
