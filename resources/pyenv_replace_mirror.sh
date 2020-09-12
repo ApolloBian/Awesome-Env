82e8d421c9ebd90e3e8ec2f63088c9f2f0630a97
Tianling@ApoMarkX.local <bian_tianling@icloud.com>
2020-09-12 18:55:46 +0800

    change anaconda mirror to tuna

--- resources/pyenv_replace_mirror.sh
+++ resources/pyenv_replace_mirror.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 cd $(pyenv root)
 pwd
-sed -i -e 's/www.python.org\/ftp/npm.taobao.org\/mirrors/g' `ack 'www.python.org/ftp' -l`
+sed -i -e 's/www.python.org\/ftp/npm.taobao.org\/mirrors/g' `ag 'www.python.org/ftp' -l`
+sed -i -e 's/repo.continuum.io/mirrors.tuna.tsinghua.edu.cn\/anaconda/g' `ag 'repo.continuum.io' -l`
 cd -
